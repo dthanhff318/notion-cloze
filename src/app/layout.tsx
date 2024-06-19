c888bcc698f8acf95e4e8314cc778538ab4e0c9e
cheft <dthanhff318@gmail.com>
2024-06-19 17:03:29 +0700

    config munti language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "~@/components/providers/themeProvider";
 import "./globals.css";
 import { ModalProvider } from "~@/components/providers/modalProvider";
 import { EdgeStoreProvider } from "~@/lib/edgestore";
+import { NextIntlClientProvider } from "next-intl";
+import { getLocale, getMessages } from "next-intl/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,27 +16,31 @@ export const metadata: Metadata = {
   description: "Workspace free for everyone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
+  const messages = await getMessages();
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
           <EdgeStoreProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="slate"
-              enableSystem
-              disableTransitionOnChange
-              storageKey="tizZote-theme"
-            >
-              <Toaster position="bottom-center" />
-              <ModalProvider />
-              {children}
-            </ThemeProvider>
+            <NextIntlClientProvider messages={messages}>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="slate"
+                enableSystem
+                disableTransitionOnChange
+                storageKey="tizZote-theme"
+              >
+                <Toaster position="bottom-center" />
+                <ModalProvider />
+                {children}
+              </ThemeProvider>
+            </NextIntlClientProvider>
           </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
