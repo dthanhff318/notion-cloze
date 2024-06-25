04dd993d1141c34026bd7d3ce6c4e91e129acd8e
linhnt <leeminho03072000@gmail.com>
2024-06-25 23:04:49 +0700

    update ui notilist

--- src/app/[locale]/(main)/_components/notisList.tsx
+++ src/app/[locale]/(main)/_components/notisList.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { ChevronsLeft } from "lucide-react";
+import { useQuery } from "convex/react";
+import { ChevronsLeft, InboxIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { Spinner } from "~@/components/spinner";
 import { useNavigation } from "~@/hooks/useNavigation";
 import { useNoti } from "~@/hooks/useNoti";
 import { cn } from "~@/lib/utils";
+import { api } from "~convex/_generated/api";
 import { translations } from "~messages/translation";
 
 const DEFAULT_WIDTH = 300;
@@ -14,58 +17,13 @@ const DEFAULT_WIDTH = 300;
 const NotiList = () => {
   const t = useTranslations();
   const isMobile = useMediaQuery("(max-width:768px)");
-  const pathname = usePathname();
   const noti = useNoti();
-  const navigation = useNavigation();
-  const isResizingRef = useRef(false);
   const notiRef = useRef<ElementRef<"div">>(null);
-  const [isReset, setIsReset] = useState(false);
-
-  const handleMouseDown = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    isResizingRef.current = true;
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-  };
-
-  const handleMouseUp = (e: MouseEvent) => {
-    isResizingRef.current = false;
-    document.removeEventListener("mousemove", handleMouseMove);
-    document.removeEventListener("mouseup", handleMouseUp);
-  };
-  const handleMouseMove = (e: MouseEvent) => {
-    if (!isResizingRef.current) return;
-    let newWidth = e.clientX - navigation.navWidth;
-    if (newWidth < DEFAULT_WIDTH) newWidth = DEFAULT_WIDTH;
-    if (newWidth > 480) newWidth = 480;
-    if (notiRef.current) {
-      notiRef.current.style.width = `${newWidth}px`;
-    }
-  };
-
-  const resetWidth = () => {
-    if (notiRef.current) {
-      noti.onOpen();
-      // setIsReset(true);
-      // notiRef.current.style.width = `${DEFAULT_WIDTH}px`;
-      // setTimeout(() => setIsReset(false), 300);
-    }
-  };
+  const notis = useQuery(api.notis.getNotis);
 
   const collapsed = () => {
     if (notiRef.current) {
-      // setIsReset(true);
       noti.onClose();
-      // setTimeout(() => {
-      // if (notiRef.current) {
-      //   notiRef.current.style.width = `${0}px`;
-      // }
-      // setIsReset(false);
-      // }, 300);
     }
   };
 
@@ -79,11 +37,10 @@ const NotiList = () => {
     <div
       ref={notiRef}
       className={cn(
-        `group/sidebar h-[100vh] bg-secondary overflow-y-hidden relative justify-between flex  flex-col z-[9999999]`,
-        isReset && "transition-all ease-in-out duration-300",
+        `group/sidebar h-[100vh] bg-secondary overflow-y-hidden relative justify-between flex  flex-col z-[999999]`,
         !noti.isOpen && "w-0 transition-all ease-in-out duration-300",
         noti.isOpen &&
-          `w-[${navigation.notiWidth}px] transition-all ease-in-out duration-300`
+          `w-[${DEFAULT_WIDTH}px] transition-all ease-in-out duration-300`
       )}
     >
       <div
@@ -97,13 +54,30 @@ const NotiList = () => {
         <ChevronsLeft className="h-6 w-6" />
       </div>
 
-      <p className="text-sm py-3 px-4 truncate">{t(translations.Noti.Title)}</p>
-
-      <div
-        className="opacity-30 hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-[3px] hover:w-1 bg-primary/10 right-0 top-0"
-        onMouseDown={handleMouseDown}
-        onClick={resetWidth}
-      ></div>
+      <p className="text-sm py-3 px-4 truncate text-muted-foreground font-medium">
+        {t(translations.Noti.Title)}
+      </p>
+      <div className={cn("flex-1", notis?.length === 0 && "flex")}>
+        {notis === undefined && (
+          <div className="h-full flex items-center justify-center p-4">
+            <Spinner size="lg" />
+          </div>
+        )}
+        {notis?.length === 0 && (
+          <div className="flex flex-col items-center justify-center gap-2 px-3">
+            <InboxIcon size={30} />
+            <p className="text-muted-foreground font-medium text-sm truncate">
+              {t(translations.Noti.No_noti_title)}
+            </p>
+            <p className="text-muted-foreground text-center text-xs line-clamp-2">
+              {t(translations.Noti.No_noti_desc)}
+            </p>
+          </div>
+        )}
+        {notis?.map((noti) => {
+          return <div></div>;
+        })}
+      </div>
     </div>
   );
 };
